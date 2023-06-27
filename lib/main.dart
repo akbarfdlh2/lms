af706790250e33702843f33daa7754e08b3e4eaf
akbarfdlh <muhamadakbarfadilah6@gmail.com>
2023-06-27 21:26:09 +0700

    Fixing Config Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:lms/ui/pages/onboarding1.dart';
 import 'package:lms/ui/pages/undefinedScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,7 +12,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 late SharedPreferences prefs;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SharedPreferences.getInstance().then((prefs) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
         .then((value) => runApp(
